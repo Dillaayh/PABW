fc7b03ea4786ab2cb852b19ba27f5d0eec05e9ca
Dilla <122979935+Dillaayh@users.noreply.github.com>
2025-04-16 04:33:25 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        deepBlue: '#203562',
+        powderBlue: '#3E588F',
+        putih: '#FFFFFF',
+        primary: '#8FAADC',
+        red: '#DE402E',
+        puple: '#E3E8F8'
+        // tambahkan warna lain sesuai kebutuhan
+      },
+    },
+  },
+  plugins: [],
+}
