8f38e5acc97b91ec319c49b8d2c63eab3e92ac0e
Dilla <122979935+Dillaayh@users.noreply.github.com>
2025-04-16 05:44:58 +0800

    page.jsx

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -68,50 +68,59 @@ const Login = () => {
     <div className="flex h-screen w-screen">
       
       {/* Form Section - Kiri */}
-      <div className="w-1/2 flex items-center justify-center bg-[#E3E8F8] p-2">
-        <div className="w-full h-full p-6 flex items-center justify-end">
-          <div className="w-full">
-            <h2 className="text-3xl font-bold mb-6 text-black text-center">Masuk ke akun anda!</h2>
+      <div className="w-1/2 flex items-center justify-center bg-[#E3E8F8] p-8 around[30px">
+        <div className="w-full h-full p-6 flex items-center justify-center">
+          <div className="w-full max-w-md bg-white  rounded-2xl shadow-lg p-8">
+            <h2 className="text-3xl font-bold mb-6 text-center text-black">Masuk ke akun anda!</h2>
             <form onSubmit={handleSubmit} className="space-y-5">
-              <div>
-                <label htmlFor='email' className="block text-sm text-center font-medium text-black">Email</label>
+              <div className="flex flex-col items-center w-full">
+                <label htmlFor="email" className="block text-sm font-medium text-black mt-8">
+                  Email
+                </label>
                 <input
                   type="text"
                   id="email"
                   name="email"
                   onChange={(e) => setEmail(e.target.value)}
-                  className="mt-1 mx-auto block w-1/2 px-4 py-2 border border-gray-300 rounded-1xl shadow-sm focus:outline-none sm:text-sm"
+                  className="mt-1 w-1/2 px-4 py-2 border border-gray-300 rounded-1xl shadow-sm focus:outline-none sm:text-sm"
                 />
               </div>
-              <div>
-                <label htmlFor='password' className="block text-sm text-center font-medium text-black">Kata Sandi</label>
-                <div className="relative">
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    name="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="mt-1 mx-auto block w-1/2 px-4 py-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none sm:text-sm"
-                  />
-                  <span
-                    className="absolute inset-y-0 right-3 flex items-center text-gray-500 cursor-pointer"
-                    onClick={togglePasswordVisibility}
-                  >
-                    {showPassword ? <FaRegEyeSlash className="w-5 h-5" /> : <FaRegEye className="w-5 h-5" />}
-                  </span>
-                </div>
+              <div className="flex flex-col items-center w-full">
+                <label htmlFor="password" className="block text-sm font-medium text-black">
+                  Kata Sandi
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 w-1/2 px-4 py-2 border border-gray-300 rounded-1xl shadow-sm focus:outline-none sm:text-sm"
+                />
+                <span
+                  className="absolute inset-y-0 right-3 flex items-center text-gray-500 cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? <FaRegEyeSlash className="w-5 h-5" /> : <FaRegEye className="w-5 h-5" />}
+                </span>
                 <p className="text-right mt-2 text-xs">
-                  <Link href="" className="text-black hover:underline">Lupa Password?</Link>
+                  <Link href="" className="text-black hover:underline">
+                    Lupa Password?
+                  </Link>
                 </p>
               </div>
-              <button
-                type="submit"
-                className="w-1/2 text-center bg-deepBlue text-putih py-2 rounded-2xl hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
-              >
-                Masuk
-              </button>
+              <div className="flex justify-center items-center">
+                <button
+                  type="submit"
+                  className="w-1/2 bg-deepBlue text-putih py-2 rounded-2xl hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                >
+                  Masuk
+                </button>
+              </div>
               <p className="text-center text-sm">
-                Belum punya akun? <Link  className="text-white font-bold hover:underline">Daftar</Link>
+                Belum punya akun?{" "}
+                <Link href="" className="text-white font-bold hover:underline">
+                  Masuk
+                </Link>
               </p>
             </form>
           </div>
@@ -119,7 +128,7 @@ const Login = () => {
       </div>
   
       {/* Gambar Section - Kanan */}
-      <div className="w-1/2 flex items-center justify-center bg-white">
+      <div className="w-[720px] flex items-center justify-center">
         <img 
           src="/images/login.png" 
           alt="login illustration" 
